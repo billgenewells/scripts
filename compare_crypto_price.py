83569add1a020bfba33da895267d794841588c6f
bill w <bill.gene.wells@gmail.com>
2017-12-30 06:54:23 -0800

    Changed to gdax provided data

--- compare_crypto_price.py
+++ compare_crypto_price.py
@@ -21,53 +21,66 @@ def cryptocompare(btcpercent, ethpercent):
     Documentation for the function
     """
 
-    yesterday = datetime.date.today()
-    timestamp = yesterday.strftime("%s")
+    #yesterday = datetime.date.today()
+   # timestamp = yesterday.strftime("%s")
 
-    coinsnow = requests.get\
-                ('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC&tsyms=USD')
+    #coinsnow = requests.get\
+    #            ('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC&tsyms=USD')
     #btcyes = requests.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=' + timestamp)
-    btcyes = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD')
-    ethyes = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD')
-    if coinsnow is None or btcyes is None or ethyes is None:
-        return "Did not get web data"
-        exit()
+    #btcyes = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD')
+    #ethyes = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD')
+    #if coinsnow is None or btcyes is None or ethyes is None:
+    #    return "Did not get web data"
+     #   exit()
 
+    btc = requests.get('https://api.gdax.com/products/BTC-USD/stats')
+    eth = requests.get('https://api.gdax.com/products/ETH-USD/stats')
+
+    btccbopenrate = float(btc.json()['open'])
+    btccbnowate = float(btc.json()['last'])
+    ethcbopenrate = float(eth.json()['open'])
+    ethcbnowate = float(eth.json()['last'])
     #yesterday = datetime.date.today()
     #timestamp = yesterday.strftime("%s")
 
-    btcnowrate = coinsnow.json()['BTC']['USD']
-    ethnowrate = coinsnow.json()['ETH']['USD']
+    #btcnowrate = coinsnow.json()['BTC']['USD']
+    #ethnowrate = coinsnow.json()['ETH']['USD']
 
-    btcyesrate = btcyes.json()['RAW']['BTC']['USD']['HIGH24HOUR']
-    ethyesrate = ethyes.json()['RAW']['ETH']['USD']['HIGH24HOUR']
+    #btcyesrate = btcyes.json()['RAW']['BTC']['USD']['HIGH24HOUR']
+    #ethyesrate = ethyes.json()['RAW']['ETH']['USD']['HIGH24HOUR']
 
-    print btcyesrate
-    print ethyesrate
+    #print btcyesrate
+    #print ethyesrate
+    print btccbopenrate
+    print ethcbopenrate
 
-    btcdiff = btcyesrate - btcnowrate
-    ethdiff = ethyesrate - ethnowrate
+    btcdiff = btccbopenrate - btccbnowate
+    ethdiff = ethcbopenrate - ethcbnowate
+    #btcdiff = btcyesrate - btcnowrate
+    #ethdiff = ethyesrate - ethnowrate
 
-    btcdifference = btcdiff/btcyesrate
-    ethdifference = ethdiff/ethyesrate
+    btcdifference = btcdiff/btccbopenrate
+    ethdifference = ethdiff/ethcbopenrate
+    #btcdifference = btcdiff/btcyesrate
+    #ethdifference = ethdiff/ethyesrate
 
-    btcmess = 'Bitcoin is currently $%s, %s%% difference' % (btcnowrate, round(btcdifference*100, 2))
-    ethmess = 'Ethereum is currently $%s, %s%% difference' % (ethnowrate, round(ethdifference*100, 2))
+    btcmess = 'Bitcoin is currently $%s, %s%% difference' % (btccbnowate, round(btcdifference*100, 2))
+    ethmess = 'Ethereum is currently $%s, %s%% difference' % (ethcbnowate, round(ethdifference*100, 2))
 
     print btcmess
     print ethmess
 
-    if btcdiff/btcyesrate >= btcpercent:
+    if btcdifference >= btcpercent:
         SNSCLIENT.publish(
             TargetArn='arn:aws:sns:us-west-2:762287401378:compare_bitcoin_price',
             Message=btcmess
         )
-    if btcnowrate > 2700:
-        SNSCLIENT.publish(
-            TargetArn='arn:aws:sns:us-west-2:762287401378:compare_bitcoin_price',
-            Message="Bitcoin is greater than 2700"
-        )
-    if ethdiff/ethyesrate >= ethpercent:
+#    if btcnowrate > 2700:
+#        SNSCLIENT.publish(
+#            TargetArn='arn:aws:sns:us-west-2:762287401378:compare_bitcoin_price',
+#            Message="Bitcoin is greater than 2700"
+#        )
+    if ethdifference >= ethpercent:
         print "sending ETH SMS"
         SNSCLIENT.publish(
             TargetArn='arn:aws:sns:us-west-2:762287401378:compare_bitcoin_price',
